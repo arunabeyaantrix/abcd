b7041f9be48c540afd9bd625b6c9ab5de904e107
ARUN DEV <arundev627@gmail.com>
2020-07-22 05:45:04 +0530

    binary

--- itertool.py
+++ itertool.py
@@ -7,4 +7,6 @@ print("Combinations \n")
 for j in range(1,int(n)+1):
 	print(*[''.join(i) for i in combinations(sorted(s),j)],sep ='\n')
 
+print(*[''.join(i) for i in combinations_with_replacement(sorted(s),int(n))],sep ='\n')
+
     
